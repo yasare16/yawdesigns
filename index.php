23363d55a38dc88ca6b8c27b60b8d64f1ad61017
Yaw Asare <yasare16@gmail.com>
2015-03-26 12:04:52 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,276 @@
+<html lang="en"><!--
+Author: Yaw Asare-Darko
+
+Date: 2012 December 1
+
+Designed for: Yaw Asare Designs
+
+Contact: yasare16@gmail.com || (347) 470-3610
+
+
+     oooo  oooo .o8888?.  oooo   ooo   oooo
+      888   888       888  888   888   888
+      888   888  88ooo888  888   888   888
+      888   888 d88   888  888   888   888      
+      `V88V'888 `o88oo888o `88o"Y88o`o"Y8'    DESIGNS!
+            888          
+            888 
+      o.    888 
+      8o.  .88'
+      `oV888V'
+        
+--><head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+   <meta name="description" content="Yaw Asare is a front-end web designer/developer based in Atlanta. Yaw Asare designs and builds affordable web sites for growing companies in Georgia. Atlanta Advertising Agency Support and Website Development Services. He is available for work in Web Design/Development, SEO and Art Direction and currently doing freelance work!">
+
+   <title>Yaw Asare | Web Designer/Developer</title>
+
+   <link rel="icon" href="/image/yaw-new-logo.png" type="image/x-icon">
+   <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+   <link rel="stylesheet" href="yawdesigns.css" type="text/css" media="all">
+   <link href="http://fonts.googleapis.com/css?family=Philosopher|Gafata|Michroma|Sanchez|Electrolize|Raleway|Fjalla+One" rel="stylesheet" type="text/css">
+   <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+   <!--[if lt IE 9]>
+       <link href="ie.css" rel="stylesheet" type="text/css" media="all">
+       <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+   <![endif]-->
+
+   <script async="" src="//www.google-analytics.com/analytics.js"></script><script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-41943233-1', 'yawdesigns.com');
+  ga('send', 'pageview');
+
+  </script>
+</head>
+
+<body> 
+    <div class="header">
+       <a href="http://www.yawdesigns.com" class="yaw-logo">
+          <img src="image/yaw-new-logo.png" alt="yaw-logo">
+       </a>
+       <div class="nav">
+         <div class="prenav">
+          <span class="d1">home</span>
+          <span class="d2">about</span>
+          <span class="d3">my work</span>
+          <span class="d4 current">contact</span>
+         </div>
+
+          <a href="#home" id="d1" class="">home</a>
+          <a href="#about" id="d2" class="">about</a>
+          <a href="#work" id="d3" class="">my work</a>
+          <a href="#contact" id="d4" class="this">contact</a>
+       </div>
+
+    </div><!--HOME SCREEN-->
+<section id="home">
+<div class="wrapper" data-stellar-ratio="0.5" style="height: 768px; left: 0px; top: 626px; display: none;">
+  <div class="container1 image"></div>
+  <div class="container2 image" style="opacity: 1;"></div>
+  <a class="showcase" href="#about" style="opacity: 0.8; color: rgb(244, 244, 244); text-shadow: none; background: rgb(17, 34, 51);">
+      <h1>yaw asare</h1>
+      <hr class="nomobi">
+      <h1>web designer &amp; developer</h1>
+  </a>
+</div>
+</section>
+
+<!--ABOUT-->
+<section id="about">
+<div class="optimum wrap">
+       <div class="profile cf">
+          <div class="blurb row">
+            <p>i'm&nbsp;&nbsp;&nbsp;<span id="about-yaw">yaw asare</span></p>
+            <p style="text-align:center">and<span id="webdesign">&nbsp;web design</span></p>
+            <p style="text-align:right;padding-top:0">is&nbsp;<span id="what">&nbsp;what&nbsp;</span>&nbsp;i do&nbsp;!</p>
+          </div>
+          <div class="me-wp nomobi">
+             <div id="me">
+               <div id="circ">
+                 <span>i'm available<br>for work!<br>•<br></span>
+                 <ul>
+                   <li>@funky4lyf</li>
+                   <li>+1(347) 470-3610</li>
+                   <li>yasare16@gmail.com</li>
+                 </ul>
+               </div>
+             </div>
+         </div>
+        </div>
+       <hr class="divider">
+      <div class="skills row">
+      <blockquote id="description">I'm an enthusiastic front-end web developer, currently in Atlanta, with a constant desire to challenge my creativity in Web Design, Web Development, User Experience(UX) and User Interface(UI).<br>With the experience I've acquired, I've come to love minimalism and clean, detailed design and incorporate it vehemently in my work!<br><br> I'm currently available for freelance and/or fulltime work.</blockquote> 
+        <p>I'm pretty awesome at:</p>
+       <div class="cf">
+        <svg class="shared" id="radial-graph" height="500">
+	   <circle cx="250" cy="250" r="200" fill="none" stroke="#a30303" stroke-width="25" stroke-dasharray="700,600" stroke-dashoffset="965"></circle>
+           <circle cx="250" cy="250" r="170" fill="none" stroke="#99c" stroke-width="25" stroke-dasharray="600,400" stroke-dashoffset="-280"></circle>
+           <circle cx="250" cy="250" r="140" fill="none" stroke="#61b33b" stroke-width="25" stroke-dasharray="600,380" stroke-dashoffset="280"></circle>
+    	   <circle cx="250" cy="250" r="110" fill="none" stroke="#37a9fa" stroke-width="25" stroke-dasharray="400,100" stroke-dashoffset="50"></circle>
+	   <circle cx="250" cy="250" r="80" stroke="#ffaf59" stroke-width="25" stroke-dasharray="443,200" stroke-dashoffset="0" fill="none"></circle>
+  	   <circle cx="250" cy="250" r="65" fill="#6a6a6a"></circle>
+           <text x="226" y="256" fill="#fff">skills</text>
+        </svg>
+        <style>circle{transition:all 0.2s}circle:hover{stroke-width:35;stroke-opacity:0.8}</style>
+        <div id="linear-graph" class="shared">
+          <p id="css">css</p>
+          <p id="htm">html</p>
+          <!--<p id="jq">jquery</p>-->
+          <p id="j">javascript</p>
+          <p id="php">php</p>
+          <p id="ph">sql</p>
+        </div>
+        
+        <div class="shared margin-top">
+        <p>and on my way to becoming a guru in:</p>
+        <ul id="guruat">
+        <li>Ruby on Rails</li>
+        <li>Python</li>
+        <li>Java</li></ul>
+       </div>
+
+      </div>
+      <a class="button resume" target="_blank" href="CV.pdf">View my résumé</a>
+     </div>
+   </div>
+</section>
+
+<!--WORK-->
+<section id="work">
+    <div class="work-container">
+       <div class="hero">
+          <div class="optimum">
+             <div class="row" style="text-shadow:0 1px #333">
+                <h1 class="page-heading">Work</h1>
+                <p>Through identity branding, web design, web development and internet consulting, below are a few projects/websites that showcase my work.</p>
+             </div>
+          </div> 
+       </div>
+       <div class="work-display">
+           <div class="items-row cf">
+               <div class="col">
+                <a href="http://organizations.mercer.edu/asa" target="_blank">
+                   <img class="first" src="/image/mercer.gif" alt="Mercer University ASA, Macon, GA/From Scratch">
+                   <img class="last" src="/image/mercer_dark.gif" alt="Mercer University ASA, Macon, GA/From Scratch">
+                </a>
+               </div>
+               <div class="col">
+               	<a href="http://www.advancedlife.org" target="_blank">
+                  <img class="first" src="/image/advanced.gif" alt="Advanced Life Organisation, Norcross, GA/WordPress">
+                  <img class="last" src="/image/adv_dark.gif" alt="Advanced Life Organisation, Norcross, GA/WordPress">
+                </a>
+               </div>
+               <div class="col">
+                <a href="http://www.lightopticalnyc.com" target="_blank">
+                  <img class="first" src="/image/light.gif" alt="Light Optical NYC/Magento">
+                  <img class="last" src="/image/light_dark.gif" alt="Light Optical NYC/Magento">
+                </a>
+               </div>
+           </div>
+
+           <div class="items-row cf">
+               <div class="col">
+                <a href="http://create.yawdesigns.com" target="_blank">
+                  <img class="first" src="/image/create.gif" alt="Create Series 2/From scratch">
+                  <img class="last" src="/image/create_dark.gif" alt="Create Series 2/From scratch">
+                </a>
+               </div>
+               <div class="col">
+               	 <a href="http://www.atrique.com" target="_blank">
+                   <img class="first" src="/image/atr.gif" alt="Atrique Clothing">
+                   <img class="last" src="/image/atr-grey.gif" alt="Atrique Clothing/South Africa/From scratch">
+                </a>
+               </div>
+               <div class="col">
+                <a href="http://idea.yawdesigns.com" target="_blank">
+                  <img class="first" src="/image/create2.gif" alt="Create Series 1/From scratch">
+                  <img class="last-one" src="/image/create2_dark.gif" alt="Create Series 1/From scratch">
+                </a>
+               </div>
+           </div>
+       </div>
+    </div>
+</section>
+
+<!--CONTACT-->
+<section id="contact" style="padding-top:60px">
+  <div class="optimum">
+       <ul class="icons row">
+          <li><a id="twitter" href="http://www.twitter.com/funky4lyf" target="_blank" title="Follow me!">twitter</a></li>
+          <li><a id="facebook" href="http://www.facebook.com/funky4lyf" target="_blank" title="Facebook me!">facebook</a></li>
+          <li><a id="skype" href="callto:djblaiz" target="_blank" title="Hit me up!">skype</a></li>
+          <li><a id="gmail" href="mailto:yasare16@gmail.com" target="_blank" title="Email me!">gmail</a></li>
+          <li><a id="soundcloud" href="http://www.soundcloud.com/yasare16" target="_blank" title="Listen to my beats!">soundcloud</a></li>
+       </ul>
+  </div>
+
+  <div class="contact">
+      <div class="div1 half-wrap">
+         <h1 class="page-heading">get in touch!</h1>
+      </div>
+      <div class="div2 half-wrap">
+         <h1 class="page-heading">send me a message!</h1>
+      </div>  
+  </div>
+  <div class="message-me">
+      <div class="optimum wrap">
+          <span class="info" id="email">yasare16@gmail.com</span>
+          <span class="info" id="phone">(+1) 374.470.3610</span> 
+      </div>
+      
+      <div class="contact-form text-shadow">
+         <form id="myForm" method="POST" action="/index.php" novalidate="novalidate">
+           <h1>Tell me about your web design project/idea, give me some feedback about this website…or just say <u>Hello!</u>☺</h1><br><br>
+           <ul>
+               <li>
+                   <label>Name<span>∗</span></label>
+                   <span><input type="text" placeholder="first name" name="f_name" value=""><input type="text" placeholder="last name" name="l_name" value=""></span>
+                   <span></span><br>
+               </li>
+               <li>  
+                   <label>Company</label>
+                   <span><input class="adjust" type="text" name="company" value=""></span>
+                   <span></span><br>
+               </li>
+               <li>
+                   <label>Email<span>∗</span></label>
+                   <span><input class="adjust small" type="email" name="email" value=""></span>
+                   <span></span><br>
+               </li>
+               <li>
+                   <label>Contact number</label>
+                   <span><input class="adjust" type="text" name="number" value=""></span>
+                   <span></span><br>
+               </li>
+               <li>
+                   <label>Subject of message<span>∗</span></label>
+                   <span><input class="adjust" type="text" name="subject" value=""></span>
+                   <span></span><br>
+               </li>
+               <li>
+                   <label>Message<span>∗</span></label>
+                   <span><textarea rows="8" class="adjust small" placeholder="…✍" name="message"></textarea></span>
+                   <span></span><br>
+               </li>
+               <li><input class="button" type="submit" name="submit" value="submit!"></li>    
+           </ul>
+                    </form>
+      </div>
+  </div>
+</section>
+<footer>
+    <div class="yaw-designs">yaw asare designs</div>
+    <div class="copy">© 2014</div>
+</footer>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="js/custom.js"></script>
+
+
+
+
+</body></html>
