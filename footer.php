01781b7aed415522b02ae0e8c4c8a58f96116f98
Yaw Asare <yasare16@gmail.com>
2016-03-28 17:58:09 -0400

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <div class="yaw-designs">yaw asare designs</div>
+    <div class="copy">&copy; <?php echo date('Y'); ?></div>
+</footer>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
+<script src="js/custom.js"></script>
+<script src="js/model-cntrl.js"></script>
+<script src="js/parallax-master/deploy/jquery.parallax.min.js"></script>
+
+</body>
+
+
+</html>
