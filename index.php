9884ba82c37b41a0521380ec13e4b5506d55dae5
Yaw Asare <yasare16@gmail.com>
2016-03-28 17:54:57 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,89 +1,18 @@
-<!DOCTYPE html>
-<html lang='en'>
-
-<!--
-Author: Yaw Asare-Darko
-
-Date: 2012 December 1
-
-Designed for: Yaw Asare Designs
-
-Contact: yasare16@gmail.com || (347) 470-3610
-
-
-     oooo  oooo .o8888?.  oooo   ooo   oooo
-      888   888       888  888   888   888
-      888   888  88ooo888  888   888   888
-      888   888 d88   888  888   888   888      
-      `V88V'888 `o88oo888o `88o"Y88o`o"Y8'    DESIGNS!
-            888          
-            888 
-      o.    888 
-      8o.  .88'
-      `oV888V'
-        
--->
-
-<head>
-   <meta charset="utf-8">
-   <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
-   <meta name="description" content="Yaw Asare is a front-end web designer/developer based in Atlanta. Yaw Asare designs and builds affordable web sites for growing companies in Georgia. Atlanta Advertising Agency Support and Website Development Services. He is available for work in Web Design/Development, SEO and Art Direction and currently doing freelance work!">
-
-   <title>Yaw Asare | Web Designer/Developer</title>
-
-   <link rel="icon" href="/image/yaw-new-logo.png" type="image/x-icon">
-   <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-   <link rel="stylesheet" href="style.css" type="text/css" media="all">
-   <link href='http://fonts.googleapis.com/css?family=Philosopher|Gafata|Michroma|Sanchez|Electrolize|Raleway|Fjalla+One' rel='stylesheet' type='text/css'>
-   <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
-   <!--[if lt IE 9]>
-       <link href="ie.css" rel="stylesheet" type="text/css" media="all">
-       <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-   <![endif]-->
-
-   <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-41943233-1', 'yawdesigns.com');
-  ga('send', 'pageview');
-
-  </script>
-</head>
-
-<body> 
-    <div class="header envelope">
-       <a href="http://www.yawdesigns.com" class="yaw-logo">
-          <img src="image/yaw-new-logo.png" alt="yaw-logo">
-       </a>
-       <div class="nav">
-         <div class="prenav">
-          <span class='d1 flap'>home</span>
-          <span class='d2 flap'>about</span>
-          <span class='d3 flap'>my work</span>
-          <span class='d4 flap'>contact</span>
-         </div>
-
-          <a href='#home' id="d1">home</a>
-          <a href='#about' id="d2">about</a>
-          <a href='#work' id="d3">my work</a>
-          <a href='#contact' id="d4">contact</a>
-       </div>
-
-</div>
+<?php include('header.php'); ?>
 <!--HOME SCREEN-->
 <section id="home">
-<div class="wrapper" data-stellar-ratio="0.5">
+<div class="wrapper">
   <div class="container1"></div>
-  <div class="container2"></div>
+  
   <div class="container3"></div>
-  <a class="showcase" href="#about">
-      <h1>yaw asare</h1>
-      <hr class="nomobi">
-      <h1>front end web developer</h1>
-  </a>
+  <a class="showcase"></a>
+  <div class="intro" data-link="#about">
+    <h1>yaw asare</h1>
+    <hr>
+    <h1>front end web developer</h1>
+    <img alt="" src="image/scroll-down.gif"><br>
+    <img alt="" src="image/scroll-down.gif" style="margin-top:-50px">
+  </div>
 </div>
 </section>
 
@@ -102,7 +31,7 @@ Contact: yasare16@gmail.com || (347) 470-3610
                <div id="circ">
                  <span>i&apos;m available<br>for work!<br>&bull;<br></span>
                  <ul>
-                   <li>@funky4lyf</li>
+                   <!--<li>@funky4lyf</li>-->
                    <li>+1(347) 470-3610</li>
                    <li>yasare16@gmail.com</li>
                  </ul>
@@ -112,7 +41,7 @@ Contact: yasare16@gmail.com || (347) 470-3610
         </div>
        <hr class="divider">
       <div class='skills cf row'>
-         <blockquote class="row shared" id="description">I'm an enthusiastic front-end/back-end web developer, currently in Atlanta, with a constant desire to challenge my creativity in Web Design, Web Development, User Experience(UX) and User Interface(UI) Design.<br>With the experience I've acquired, I've come to love minimalism and clean, detailed design and incorporate it vehemently in my work!<br><br> I'm currently available for freelance and/or fulltime work.
+         <blockquote class="row shared" id="description">I'm an enthusiastic front-end/back-end web developer, currently in Atlanta, with a constant desire to challenge my creativity in Web Design, Web Development, User Experience(UX) and User Interface(UI) Design.<br>With the experience I've acquired, I've come to love minimalist, clean, detailed design and incorporate it vehemently in my work!<br><br> I'm currently available for freelance, contract or full time work.
        <hr class="divider margin-top">
        <div class=" nomobi margin-top">
         <p style="text-align:center">...and on my way to becoming a guru in:</p>
@@ -187,7 +116,7 @@ Contact: yasare16@gmail.com || (347) 470-3610
   <div class="optimum">
        <ul class="icons row">
           <li><a id="twitter" href="http://www.twitter.com/funky4lyf" target="_blank" title="Follow me!">twitter</a></li>
-          <li><a id="facebook" href="http://www.facebook.com/funky4lyf" target="_blank" title="Facebook me!">facebook</a></li>
+          <!--<li><a id="facebook" href="http://www.facebook.com/funky4lyf" target="_blank" title="Facebook me!">facebook</a></li>-->
           <li><a id="skype" href="callto:djblaiz" target="_blank" title="Hit me up!">skype</a></li>
           <li><a id="gmail" href="mailto:yasare16@gmail.com" target="_blank" title="Email me!">gmail</a></li>
           <li><a id="soundcloud" href="http://www.soundcloud.com/yasare16" target="_blank" title="Listen to my beats!">soundcloud</a></li>
@@ -214,47 +143,40 @@ Contact: yasare16@gmail.com || (347) 470-3610
            <ul>
                <li>
                    <label>Name<span>&lowast;</span></label>
-                   <span><input type="text" placeholder="first name" name="f_name" value=""><input type="text" placeholder="last name" name="l_name" value="<?php echo $l; ?>"></span>
-                   <span><?php $l; ?></span><br>
+                   <span><input type="text" name="f_name"></span><br>
                </li>
                <li>  
                    <label>Company</label>
-                   <span><input class="adjust" type="text" name="company" value="<?php echo $c; ?>"></span>
-                   <span><?php $c; ?></span><br>
+                   <span><input type="text" name="company"></span><br>
                </li>
                <li>
                    <label>Email<span>&lowast;</span></label>
-                   <span ><input class="adjust small" type="email" name="email" value="<?php echo $e; ?>"></span>
-                   <span><?php $e; ?></span><br>
+                   <span ><input type="text" name="email"></span><br>
                </li>
                <li>
                    <label>Contact number</label>
-                   <span><input class="adjust" type="text" name="number" value="<?php echo $n; ?>"></span>
-                   <span><?php $n; ?></span><br>
+                   <span><input type="text" name="number"></span><br>
                </li>
                <li>
                    <label>Subject of message<span>&lowast;</span></label>
-                   <span><input class="adjust" type="text" name="subject" value="<?php echo $s; ?>"></span>
-                   <span><?php $s; ?></span><br>
+                   <span><input type="text" name="subject"></span><br>
                </li>
                <li>
                    <label>Message<span>&lowast;</span></label>
-                   <span><textarea rows="8" class="adjust small" placeholder="&hellip;&#9997;" name="message"></textarea></span>
-                   <span><?php $m; ?></span><br>
+                   <span><textarea rows="8" placeholder="&hellip;&#9997;" name="message"></textarea></span><br>
                </li>
                <li><input class="button" type="submit" name="submit" value="submit!"></li>    
            </ul>
            <?php
 
          if($_SERVER['REQUEST_METHOD'] == 'POST'){
-            $f = $_POST['f_name'];
-            $l = $_POST['l_name'];
+            $fn = $_POST['f_name'];
             $c = $_POST['company'];
             $e = $_POST['email'];
             $n = $_POST['number'];
             $s = $_POST['subject'];
             $m = $_POST['message'];
-            $mes_sage = "<strong>Name:</strong> $f $l<br>
+            $message = "<strong>Name:</strong> $fn<br>
                         <strong>Company:</strong> $c<br><strong>Sent from:</strong> $e<br>
                         <strong>Phone number:</strong> $n<br>
                         <strong>Message:</strong> $m<br><br>
@@ -267,9 +189,9 @@ Contact: yasare16@gmail.com || (347) 470-3610
 		  	    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
 
-          	$r = mail($to, $s, $mes_sage, $headers) ? "<p class='success'>&#10004; Your message has been sent!</p>" : "<p class='failure'>&#10006; Sorry, there was an error sending the message. Try again later.</p>";
+          	$r = mail($to, $s, $message, $headers) ? "<p class='success'>&#10004; Your message has been sent!</p>" : "<p class='failure'>&#10006; Sorry, there was an error sending the message. Try again later.</p>";
           	echo $r;
-          	header('Location:http://www.yawdesigns.com/index.php');
+          	header('Location:http://www.yawdesigns.com/index.php#contact');
          }// end
 
        ?>
@@ -277,15 +199,4 @@ Contact: yasare16@gmail.com || (347) 470-3610
       </div>
   </div>
 </section>
-<!--FOOTER-->
-<footer>
-    <div class="yaw-designs">yaw asare designs</div>
-    <div class="copy">&copy; <?php echo date('Y'); ?></div>
-</footer>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
-<script src="js/custom.js"></script>
-<script src="js/model-cntrl.js"></script>
-
-</body>
-</html>
+<?php include('footer.php'); ?>
