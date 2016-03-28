6b55c38da73193607c9a6d20b3da4f7c61654178
Yaw Asare <yasare16@gmail.com>
2016-03-28 17:57:31 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<!--
+Author: Yaw Asare-Darko
+
+Date: 2012 December 1
+
+Designed for: Yaw Asare Designs
+
+Contact: yasare16@gmail.com || (347) 470-3610
+
+
+     oooo  oooo .o8888?.  oooo   ooo   oooo
+      888   888       888  888   888   888
+      888   888  88ooo888  888   888   888
+      888   888 d88   888  888   888   888      
+      `V88V'888 `o88oo888o `88o"Y88o`o"Y8'    DESIGNS!
+            888          
+            888 
+      o.    888 
+      8o.  .88'
+      `oV888V'
+        
+-->
+
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+   <meta name="description" content="Yaw Asare is a front-end web designer/developer based in Atlanta. Yaw Asare designs and builds affordable web sites for growing companies in Georgia. Atlanta Advertising Agency Support and Website Development Services. He is available for work in Web Design/Development, SEO and Art Direction and currently doing freelance work!">
+
+   <title>Yaw Asare | Web Designer/Developer</title>
+
+   <link rel="icon" href="/image/yaw-new-logo.png" type="image/x-icon">
+   <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+   <link rel="stylesheet" href="style.css" type="text/css" media="all">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+   <link href='http://fonts.googleapis.com/css?family=Philosopher|Gafata|Electrolize|Fjalla+One' rel='stylesheet' type='text/css'>
+   <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+   <!--[if lt IE 9]>
+       <link href="ie.css" rel="stylesheet" type="text/css" media="all">
+       <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+   <![endif]-->
+
+   <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-41943233-1', 'yawdesigns.com');
+  ga('send', 'pageview');
+
+  </script>
+</head>
+
+<body> 
+    <div class="header envelope">
+       <a href="#home" class="yaw-logo layer" data-depth="0.10">
+          <img src="image/yaw-new-logo.png" alt="yaw-logo">
+       </a>
+       <div class="nav">
+         <div class="prenav">
+          <span class='d1 flap'>home</span>
+          <span class='d2 flap'>about</span>
+          <span class='d3 flap'>my work</span>
+          <span class='d4 flap'>contact</span>
+         </div>
+
+          <a href='#home' id="d1"><i class="fa fa-home"></i>home</a>
+          <a href='#about' id="d2"><i class="fa fa-info-circle"></i>about</a>
+          <a href='#work' id="d3"><i class="fa fa-laptop"></i>my work</a>
+          <a href='#contact' id="d4"><i class="fa fa-envelope"></i>contact</a>
+       </div>
+
+    </div>
